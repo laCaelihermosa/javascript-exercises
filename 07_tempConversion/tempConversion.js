dde91ebd20f4a6fd23f1c54e4665872082a94389
laCaelihermosa <fabian_lr0505@outlook.com>
2023-03-27 13:53:43 -0600

    TDD solved practice, exercises from 1 to 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitBasis) {
+  let fahrenheitTemp = fahrenheitBasis;
+  let fToc = Math.round(((fahrenheitTemp - 32) * 5 / 9) * 10) /10;
+  return fToc;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusBasis) {
+  let celsiusTemp = celsiusBasis;
+  let cTof = Math.round((celsiusTemp * 9 /5 + 32) * 10) /10;
+  return cTof;
 };
 
 // Do not edit below this line
